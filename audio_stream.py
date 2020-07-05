c29ab20b8b7be223cf0c5817e14c8c9b50ce9d7b
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-07-05 22:07:25 +0530

    Audio Stream Output

---
+++ audio_stream.py
@@ -0,0 +1,4 @@
+<Stream: itag="140" mime_type="audio/mp4" abr="128kbps" acodec="mp4a.40.2" progressive="False" type="audio">
+<Stream: itag="249" mime_type="audio/webm" abr="50kbps" acodec="opus" progressive="False" type="audio">
+<Stream: itag="250" mime_type="audio/webm" abr="70kbps" acodec="opus" progressive="False" type="audio">
+<Stream: itag="251" mime_type="audio/webm" abr="160kbps" acodec="opus" progressive="False" type="audio">
