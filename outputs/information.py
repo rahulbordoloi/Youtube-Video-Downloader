9e1ec26315f28456231e48fef64edd5ee766b203
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-07-05 22:08:40 +0530

    Video URL Information

---
+++ outputs/information.py
@@ -0,0 +1,9 @@
+Title :  Narcos - Main Trailer - Netflix [HD]
+Views :  1009961
+Length of video:  128 seconds
+Description:  The Medellin cartel - the most violent, ruthless and wealthy criminal organization in the history of modern crime. And the one man who lorded over them all... Pablo Escobar.
+
+NARCOS is the true story of the U.S. and Colombian efforts to battle the Medellin cartel during the cocaine-fueled drug wars of the 1980's. Multi-layered and multi-faceted, NARCOS depicts a brutal world where lines are blurred and little is black and white. The Netflix original series, starring Wagner Moura, Boyd Holbrook, and Pedro Pascal.
+
+http://www.netflix.com
+Ratings:  4.9310346
